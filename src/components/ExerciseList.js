6f935816dd95cbce3ba6eb57c9d04e0d394635c4
fernancumez <fernancumez@gmail.com>
2020-08-18 19:16:05 -0600

    Se creo un componente para renderizar todos los ejercicios existentes

---
+++ src/components/ExerciseList.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+import Card from "./Card";
+
+const ExerciseList = ({ exercises }) => (
+  <Fragment>
+    {exercises.map((exercise) => (
+      <Card
+        key={exercise.id}
+        title={exercise.title}
+        description={exercise.description}
+        img={exercise.img}
+        leftColor={exercise.leftColor}
+        rightColor={exercise.rightColor}
+      />
+    ))}
+  </Fragment>
+);
+
+export default ExerciseList;
