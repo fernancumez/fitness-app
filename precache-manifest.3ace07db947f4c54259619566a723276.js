20cfd211b91bd0e17a6b0d6199e053a317853be7
fernancumez <fernancumez@gmail.com>
2020-12-08 21:17:12 -0600

    Updates

---
+++ precache-manifest.3ace07db947f4c54259619566a723276.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5739b62315fa806a6a21f30ce7e9216",
+    "url": "/fitness-app/index.html"
+  },
+  {
+    "revision": "5a40501dfe31205f7ec1",
+    "url": "/fitness-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "fa4787d43d24de2f9c87",
+    "url": "/fitness-app/static/css/main.74e8cedd.chunk.css"
+  },
+  {
+    "revision": "5a40501dfe31205f7ec1",
+    "url": "/fitness-app/static/js/2.8a36cc72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fitness-app/static/js/2.8a36cc72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa4787d43d24de2f9c87",
+    "url": "/fitness-app/static/js/main.cdd2271f.chunk.js"
+  },
+  {
+    "revision": "06f7c187912db07bb976",
+    "url": "/fitness-app/static/js/runtime-main.7f20808d.js"
+  },
+  {
+    "revision": "854842fa495e13de2d54968cec61b6d1",
+    "url": "/fitness-app/static/media/500.854842fa.png"
+  },
+  {
+    "revision": "eb728687589f75caecdb7f2b3d71c1d5",
+    "url": "/fitness-app/static/media/circles.eb728687.png"
+  }
+]);
